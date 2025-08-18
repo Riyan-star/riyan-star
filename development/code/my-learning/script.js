dd91dfc0a47d67b575cce3c2169992955fb8bb8a
+ <rgigs123@gmail.com>
2025-08-18 14:01:08 +0300

    script.js

---
+++ development/code/my-learning/script.js
@@ -0,0 +1,11 @@
+let country = "Guinea Bissau";
+let newCountry = country.replace(/ /g, "-");
+console.log(newCountry);
+
+let city = "sydney";
+let newCity = city.replace(/ /g, "-");
+console.log(newCity);
+
+let attraction = "Opera House";
+let newAttraction = attraction.toLowerCase();
+console.log(newAttraction);
