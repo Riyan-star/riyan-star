cf8522875628bc4ba9b1ec809e3bdc99d9187849
+ <rgigs123@gmail.com>
2025-08-19 11:36:23 +0300

    index.js

--- development/code/my-learning/script.js
+++ development/code/my-learning/script.js
@@ -1,11 +1,11 @@
-let country = "Guinea Bissau";
-let newCountry = country.replace(/ /g, "-");
-console.log(newCountry);
+// Given a float 4.5, round it up and console log it.
+let num = 4.5;
+console.log(Math.ceil(num)); 
+//Given a float 4.5, round it down and console log it.
+let newNum = 4.5;
+console.log(Math.floor(newNum)); 
+//Console log the minimum value of 4, 1, 3, 10.
+console.log(Math.min(4, 1, 3, 10)); 
+//If you also want to get the maximum value, you can use:
+console.log(Math.max(4, 1, 3, 10));
 
-let city = "sydney";
-let newCity = city.replace(/ /g, "-");
-console.log(newCity);
-
-let attraction = "Opera House";
-let newAttraction = attraction.toLowerCase();
-console.log(newAttraction);
