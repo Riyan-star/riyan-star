b48d9ec902bedf28e281d09c87711229d4d35aad
+ <rgigs123@gmail.com>
2025-08-21 13:39:02 +0300

    index.js

---
+++ development/code/my-learning/weather-project/index.js
@@ -0,0 +1,40 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20
+  }
+};
+
+let city = prompt("Enter a city");
+if (city) {
+  city = city.toLowerCase(); // make case-insensitive
+  if (weather[city]) {
+    let celsius = Math.round(weather[city].temp);
+    let fahrenheit = Math.round((celsius * 9) / 5 + 32);
+    let humidity = Math.round(weather[city].humidity);
+
+    alert(
+      `It is currently ${celsius}°C (${fahrenheit}°F) in ${city[0].toUpperCase() + city.slice(1)} with a humidity of ${humidity}%`
+    );
+  } else {
+    alert(
+      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+    );
+  }
+}
